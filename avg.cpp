900f64f0754fd81e2c963d318bf609b87e80dec3
Aleksandr Isk <aleksandriskakov7@gmail.com>
2025-01-21 10:31:38 +0200

    Update avg.cpp

--- avg.cpp
+++ avg.cpp
@@ -1,8 +1,18 @@
-
 #include <iostream>
 using namespace std;
 int main()
 {
-    double a;
-    cout << "1st number" << endl;
-        cin >> a;
+double a, b;
+
+cout << "1st number: " << endl
+cin >> a;
+
+cout << "2st number: " << endl
+cin >> b;
+
+double avg = (a+b)/2;
+cout << avg << endl;
+
+
+return 0;
+}
